41966c8ef01a52926426dc39ee1159cd2d303ed9
Nagisetti Charan <136823650+CharanNagisetti@users.noreply.github.com>
2025-05-26 19:41:42 +0530

    Time: 2 ms (99.23%) | Memory: 42.2 MB (66.35%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        int open = 0; // balance factor
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                if(open > 0){
+                    result.append(c);
+                }
+                open++;
+            }
+            else if(c == ')'){
+                open--;
+                if(open > 0){
+                    result.append(c);
+                }
+            }
+        }
+        return result.toString();
+    }
+}
