90f498086334b484dd6562cdb08ad0d7f058d1ec
Nagisetti Charan <136823650+CharanNagisetti@users.noreply.github.com>
2025-08-22 22:15:46 +0530

    Time: 2 ms (83.63%) | Memory: 45.8 MB (43.10%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(i>count){
+                return false;
+            }
+            count=Math.max(count,i+nums[i]);
+            if(count>=n-1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
