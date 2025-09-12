1e43be60ccdccf716e690105e09d428916d97f9d
Nagisetti Charan <136823650+CharanNagisetti@users.noreply.github.com>
2025-09-12 12:18:30 +0530

    Time: 7 ms (55.38%) | Memory: 45.7 MB (45.41%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -12,11 +12,10 @@ class Solution {
     private int findVowels(String s){
         int count=0;
         for(char c: s.toCharArray()){
-           if (check(c))count++;
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                count++;
+            }
         }
         return count;
-    }
-    private boolean check(char s){
-        return "aeiouAEIOU".indexOf(s)>=0;
     }
 }
