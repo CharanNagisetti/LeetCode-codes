5a236a5af6c0128a80605dbc7bcfdba717e66b5c
Nagisetti Charan <136823650+CharanNagisetti@users.noreply.github.com>
2025-10-16 13:53:55 +0530

    Time: 1 ms (99.52%) | Memory: 46.7 MB (52.86%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] countBits(int n) {
         int[] dp=new int[n+1];
-        Arrays.fill(dp,0);
+        dp[0]=0;
         for(int i=0;i<=n;i++){
             dp[i]=dp[i>>1]+(i&1);
         }
