fd3cfecbe55311795c521c55bfdf86988997df81
Nagisetti Charan <136823650+CharanNagisetti@users.noreply.github.com>
2025-10-19 09:11:52 +0530

    Time: 316 ms (5.62%) | Memory: 50.1 MB (43.82%) - LeetSync

---
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String findLexSmallestString(String s, int a, int b) {
+        Set<String> set=new HashSet<>();
+        Queue<String> q=new LinkedList<>();
+        String ans=s;
+        q.offer(s);
+        set.add(s);
+        while(!q.isEmpty()){
+            String curr=q.poll();
+            if(curr.compareTo(ans)<0) ans=curr;
+            char[] chars = curr.toCharArray();
+            for(int i=1;i<chars.length;i+=2){
+                chars[i]=(char)(((chars[i]-'0'+a)%10)+'0');
+            }
+            String add=new String(chars);
+            String rotated=curr.substring(curr.length()-b)+curr.substring(0,curr.length() - b);
+            System.out.print(add+" "+rotated+"  ");
+            if(set.add(add)) q.offer(add);
+            if(set.add(rotated)) q.offer(rotated);
+        }
+        return ans;
+    }
+}
