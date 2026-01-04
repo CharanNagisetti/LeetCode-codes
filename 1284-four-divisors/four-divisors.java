7787b527c2ea1e67a48ac1849d393420bf37b00b
Nagisetti Charan <136823650+CharanNagisetti@users.noreply.github.com>
2026-01-04 11:22:47 +0530

    Time: 9 ms (97.47%) | Memory: 46.4 MB (51.01%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -1,28 +1,25 @@
 class Solution {
-    public ArrayList<Integer> findDivisors(int x){
-        ArrayList<Integer> arr=new ArrayList<>();
-        for(int i=1;i*i<=x;i++){
-            if(x%i==0){
-                if(i==(x/i)) arr.add(i);
-                else{
-                    arr.add(i);
-                    arr.add(x/i);
-                }
-            }
-        }
-        return arr;
-    }
     public int sumFourDivisors(int[] nums) {
         int n=nums.length;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            ArrayList<Integer> arr=findDivisors(nums[i]);
-            if(arr.size()==4){
-                for(int x: arr){
-                    sum+=x;
+        int ans=0;
+        for(int x: nums){
+            int curr=0;
+            int cnt=0;
+            for(int i=1;i*i<=x;i++){
+                if(x%i==0){
+                    if(i==x/i){
+                        curr+=i;
+                        cnt+=1;
+                    }else{
+                        curr+=i;
+                        curr+=x/i;
+                        cnt+=2;
+                    }
                 }
+                if(cnt>4) break;
             }
+            if(cnt==4) ans+=curr;
         }
-        return sum;
+        return ans;
     }
 }
