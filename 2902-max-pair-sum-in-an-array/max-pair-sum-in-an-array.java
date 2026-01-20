0b91f53625a7a43bd18cea61f76b49ac4a0f8b68
Nagisetti Charan <136823650+CharanNagisetti@users.noreply.github.com>
2026-01-20 21:30:59 +0530

    Time: 8 ms (44.99%) | Memory: 46.8 MB (28.65%) - LeetSync

--- 2902-max-pair-sum-in-an-array/max-pair-sum-in-an-array.java
+++ 2902-max-pair-sum-in-an-array/max-pair-sum-in-an-array.java
@@ -1,29 +1,37 @@
 class Solution {
     public int maxSum(int[] nums) {
+        Map<Integer, List<Integer>> map=new HashMap<>();
         int n=nums.length;
-        int[] highest=new int[n];
         for(int i=0;i<n;i++){
+            int highest=0;
             int x=nums[i];
-            int high=0;
             while(x>0){
-                int temp=x%10;
+                highest=Math.max(highest,x%10);
                 x=x/10;
-                high=Math.max(high,temp);
             }
-            highest[i]=high;
-        }
-        for(int i=0;i<n;i++){
-            System.out.print(highest[i]+" ");
+            if(!map.containsKey(highest)){
+                map.put(highest,new ArrayList<>());
+            }
+            map.get(highest).add(nums[i]);
         }
         int ans=-1;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(highest[i]==highest[j]){
-                    int temp=nums[i]+nums[j];
-                    System.out.print(nums[i]+" "+nums[j]+"   ");
-                    ans=Math.max(ans,temp);
+        for(int x: map.keySet()){
+            List<Integer> list=map.get(x);
+            int size=list.size();
+            if(size<2) continue;
+            int sum=0;
+            int max1=Integer.MIN_VALUE;
+            int max2=Integer.MIN_VALUE;
+            for(int y: list){
+                if(y>max1){
+                    max2=max1;
+                    max1=y;
+                }else if(y>max2){
+                    max2=y;
                 }
             }
+            sum+=max1+max2;
+            ans=Math.max(ans,sum);
         }
         return ans;
     }
